27170f0e820d06b3e97a4a76d1825ca13104455b
Darryll Robinson <darryll@stillproud.com>
2021-05-27 16:59:19 +1000

    Outcome notes should be working too

--- src/features/collections/CollectionForm.js
+++ src/features/collections/CollectionForm.js
@@ -725,7 +725,7 @@ export const CollectionForm = (props) => {
             error={state.fields.entities['newCaseNotes'].error}
             id="form-input-control-newCaseNotes"
             name="newCaseNotes"
-            label="New Case Note"
+            label="New Case Notes"
             onChange={handleChange}
             type="text"
             value={state.fields.entities['newCaseNotes'].value}
